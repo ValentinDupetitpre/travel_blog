144baa222d9ebd9de6e17806b341289a540c0d5a
ValentinDupetitpre <valentin.dupetitpre@gmail.com>
2020-01-27 09:42:52 +0100

    better mobile view

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,10 +5,18 @@ import './Footer.css'
 const Footer = (props) => {
 
     return(
-        <div className="footer-wrapper">
-            <img className="insta" alt="instagram" src="https://firebasestorage.googleapis.com/v0/b/travel-blog-f8f0d.appspot.com/o/home%2Finstagram.png?alt=media&token=17ebac78-e315-46aa-941c-c5c03241ddf2"/>
-            <div className="copyright">© Copyrights</div>
-        </div>
+        <footer className="footer-wrapper">
+            {/* <div className="pictos"> */}
+                <a href="https://www.instagram.com/lngvhelene/?hl=fr" target="_blank">
+                    <img className="insta" alt="instagram" src="https://firebasestorage.googleapis.com/v0/b/travel-blog-f8f0d.appspot.com/o/home%2Finstagram.png?alt=media&token=17ebac78-e315-46aa-941c-c5c03241ddf2"/>
+                </a>
+                <div className="email">
+                    <img alt="email" src="https://firebasestorage.googleapis.com/v0/b/travel-blog-f8f0d.appspot.com/o/home%2Femail.svg?alt=media&token=180f7710-2480-4cd3-aa5e-b7f1dd70131c"/>
+                    valentin.dupetitpre@gmail.com
+                </div>
+            {/* </div> */}
+            <p className="copyright">© Copyrights</p>
+        </footer>
     )
 }
 
