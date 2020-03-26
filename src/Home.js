6bf5447cb65418090c215c0e89822138d2bd0fdb
ValentinDupetitpre <valentin.dupetitpre@gmail.com>
2020-03-27 11:24:51 +1300

    update homepage info message

--- src/Home.js
+++ src/Home.js
@@ -29,6 +29,18 @@ const Home = () => {
             </div>
           </div>
         </section>
+        <div className="general-info">
+          <h3>Informations générales : nous indiquerons ici notre situation de vie quotidienne</h3>
+          <p>Aujourd'hui lundi 23 Mars 2020 à 13h30, Jacinda Ardern, première ministre Néo-Zélandaise a indiqué un passage au niveau d'alerte 3 immédiatement et un passage au niveau 4 (sur 4) dans les 48h. 
+            Cela veut dire que nous devons nous confiner totalement ici et que seuls les commerces nécessaires resteront ouvert. Ils resteront ravitaillés tout le long du confinement. Nous avons trouvé en dernière
+            minute une maison dans la banlieue d'Auckland. Nous avons beaucoup de chance car la maison est grande et rien que pour nous pour pas trop cher. Le confinement total en Nouvelle Zélande implique que l'on peut sortir tant qu'on reste éloignés des autres personnes.
+          </p>
+          <p>Pas d'inquiétudes à avoir pour nous ! Nous sommes en bonne santé, dans une grande maison. 
+            Nous sommes juste un peu frustré de ne pas pouvoir parcourir ce beau pays pour l'instant et de dépenser de l'argent pour un loyer pendant un mois.
+          </p>
+          <p>Si vous voulez davantage d'informations n'hésitez pas à nous écrire ;)
+          </p>
+        </div>
         <Preview />
     </div>
   )
